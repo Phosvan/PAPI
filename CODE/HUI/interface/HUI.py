8805a67b831b078ac2a14dc251b61b3a88ab3349
JarredTD <jarred.desrosiers@yahoo.com>
2022-09-15 23:51:24 -0400

    Rework widget logic, add functionality of input parsing and restructuring.
    WidgetSet class was reworked to take in input in pairings. This would be a pill name, and quantity. This is to allow for displaying of said pairing onto the screen. A total of 8 pairings are allowed to be taken in as a "prescription" and is then parsed to pass into the class. Upon creating the widgets, the parsed prescription is then re-parsed to be sent back out.

--- CODE/HUI/interface/HUI.py
+++ CODE/HUI/interface/HUI.py
@@ -1,21 +1,23 @@
+from cProfile import label
 import tkinter as tk
 
 # A set of widgets, said widgets are what show when 
 class WidgetSet():
-    def __init__(self) -> None:
-        self.cnt = tk.IntVar()
+    def __init__(self, pair) -> None:
+        self.pill_name = pair[0]
+        self.pill_amount = pair[1]
 
         self.frame = tk.Frame(root,borderwidth=0, relief="groove")
         self.frame.pack(side="top", fill="x", pady=(12, 0))
 
-        self.entry = tk.Label(self.frame ,width=30, font=('Arial 16'), bg="white")
-        self.entry.pack(side="left", padx=(100,0))
+        self.entry = tk.Label(self.frame ,width=30, text=self.pill_name, font=('Arial 16'), bg="white")
+        self.entry.pack(side="left", padx=(150,0))
 
         self.inner_frame = tk.Frame(self.frame, borderwidth=0, relief="groove")
         self.inner_frame.pack(side="right", fill="x")
         
-        self.label = tk.Label(self.inner_frame, width=5, textvariable=self.cnt)
-        self.label.pack(side="left")
+        self.label = tk.Label(self.inner_frame, width=5,height=2, text=self.pill_amount, bg="white")
+        self.label.pack(side="left", padx=(0,250))
 
     def Minus(self):
         if self.cnt.get() > 0:
@@ -25,14 +27,15 @@ class WidgetSet():
         if self.cnt.get() < 99:
             self.cnt.set(self.cnt.get()+1)
 
-def output_data():
-    for wSet in obj:
-        if wSet.entry.get() != "" and wSet.cnt.get() != 0:
-            print(f"{wSet.entry.get()},{wSet.cnt.get()}")
+def output_data(packet):
+    print(packet)
 
-def createwidgets():
+def createwidgets(pair):
     if len(obj) < 8:
-        obj.append(WidgetSet())
+        obj.append(WidgetSet(pair))
+
+def clean_widgets():
+    pass
 
 def removewidgets():
     if len(obj) > 1:
@@ -48,12 +51,31 @@ def removewidgets():
         frame_deleter(root_frame)
         root_frame.destroy()
 
+def prepare_packet(packet):
+    string_arr = []
+    for element in packet:
+        if type(element) == list:
+            element = prepare_packet(element)
+        string_arr.append(element)
+    
+    return ",".join(string_arr)
+
 obj = []
-def hui_main():
-    root = tk.Tk()
+root = tk.Tk()
+def hui_main(packet):
+
+
     root.geometry("800x480")
 
-    createWidgetButton = tk.Button(root, height=2, text="Done", command=output_data)
+    name_label = tk.Label(root, text=packet[0])
+    name_label.pack(side="top")
+
+    for pair in packet[1::]:
+        createwidgets(pair)
+
+    string_packet = prepare_packet(packet)
+
+    createWidgetButton = tk.Button(root, height=2, text="Done", command=lambda: output_data(string_packet))
     createWidgetButton.pack(side="bottom", fill="x")
 
 
--- CODE/HUI/main.py
+++ CODE/HUI/main.py
@@ -1,5 +1,25 @@
 from qr_reader import reader
 from interface import HUI
 
-reader.read_main()
 
+def parse_string(prescription_string):
+    prescription_list = prescription_string.split(',')
+
+    prescription_name = prescription_list.pop(0)
+    prescription_pairs = [prescription_list[i:i + 2] for i in range(0, len(prescription_list), 2)]
+
+    prescription_list = [prescription_name]
+    for pair in prescription_pairs:
+        prescription_list.append(pair)
+
+        if len(prescription_list) == 9:
+            break
+
+    return prescription_list
+
+def main():
+    prescription_string = "Abigale Rhodes,Pill 1,20,Pill 1,20,Pill 1,20,Pill 1,20,Pill 1,20,Pill 1,20,Pill 1,20,Pill 1,20,Pill 1,20"
+    packet = parse_string(prescription_string)
+    HUI.hui_main(packet)
+
+main()
