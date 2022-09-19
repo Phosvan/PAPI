e3f44b78b14d0c69874fd9423f72af55f5795a21
JarredTD <jarred.desrosiers@yahoo.com>
2022-09-19 19:17:03 -0400

    Add dimensions for gui

--- CODE/HUI/libs/Pages.py
+++ CODE/HUI/libs/Pages.py
@@ -5,6 +5,7 @@ class Controller(tk.Tk):
 
     def __init__(self):
         tk.Tk.__init__(self)
+        self.geometry("800x480")
 
         # root container
         container = tk.Frame(self)
@@ -53,37 +54,3 @@ class DisplayPage(tk.Frame):
         button = tk.Button(self, text="Go to the Scan",
                            command=lambda: controller.show_frame("ScanPage"))
         button.pack()
-
-
-
-
-
-# class Window:
-#     scan_widgets = []
-#     display_widgets = []
-#     def __init__(self) -> None:
-#         self.root = tk.Tk()
-#         self.root.geometry("800x480")
-
-
-#         self.scan_frame = tk.Frame(self.root)
-#         self.scan_frame.pack(fill="x")
-
-#         self.display_frame = tk.Frame(self.root)
-#         self.display_frame.pack(fill="x")
-
-#     def create_WidgetSet(self, prescription_pair):
-#         if len(WidgetSet.widget_sets) < 8:
-#             WidgetSet.widget_sets.append((WidgetSet(prescription_pair, self.root )))
-
-#     def destroy_WidgetSets(self):
-#         for widget in WidgetSet.widget_sets:
-#             widget.destroy()
-
-#     def scan_input(self):
-#         self.scan_frame.tkraise()
-#         self.scan_widgets.append(tk.Label(text="Scan"))
-
-#     def display(self):
-#         self.display_frame.tkraise()
-#         self.scan_widgets.append(tk.Label(text="Scan"))
