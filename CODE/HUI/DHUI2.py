574f2515e6f13cabe739aca074aae185f054ceb8
doobshub <117489665+doobshub@users.noreply.github.com>
2022-11-04 20:17:35 -0400

    Update DHUI2.py

--- CODE/HUI/DHUI2.py
+++ CODE/HUI/DHUI2.py
@@ -41,7 +41,7 @@ class start(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg= "#d459de")
         self.controller = controller
-        label = tk.Label (self, text= "Welcome to PAPI, Please Scan QR", width= 200, height= 300, font= ("Comic Sans Ms", 50), bg= "#d459de"     
+        label = tk.Label (self, text= "Welcome to PAPI, Please Scan QR", width= 200, height= 300, font= ("Comic Sans Ms", 50), bg= "#d459de")    
         controller.attributes('-fullscreen', True)
         label.pack(side="top", fill= "x", pady=10)   
 
