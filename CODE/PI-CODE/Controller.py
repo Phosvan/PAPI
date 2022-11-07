71ec6a395855b862c6f1f41c264c9dc0ba0a4f10
JarredTD <jarred.desrosiers@yahoo.com>
2022-11-06 23:06:05 -0500

    huh

--- CODE/PI-CODE/Controller.py
+++ CODE/PI-CODE/Controller.py
@@ -1,71 +1,96 @@
 from multiprocessing import *
 from ControllerClass import HuiController
 from ControllerClass import PiController
-from ControllerClass import Controller
 
-scan_input_bool = False
-listen_bool = False
-speak_bool = False
+# scan_input_bool = False
+# listen_bool = False
+# speak_bool = False
     
-scan_input_data = None
-listen_data = None
+# scan_input_data = None
+# listen_data = None
 
 
-def PiFunction():
+# def PiFunction():
 
-    global scan_input_bool
-    global listen_bool
-    global speak_bool
-    global listen_data
-    global scan_input_data
+#     global scan_input_bool
+#     global listen_bool
+#     global speak_bool
+#     global listen_data
+#     global scan_input_data
 
-    PI = PiController()
-    while True:
-        print(scan_input_bool)
-        if scan_input_bool is True and scan_input_data is None:
-            scan_input_data = PI.scan_input()
-            #scan_input_bool = False
+#     PI = PiController()
+#     while True:
+#         print(scan_input_bool)
+#         if scan_input_bool is True and scan_input_data is None:
+#             scan_input_data = PI.scan_input()
+#             scan_input_bool = False
         
-        # if PI.listen_bool is True:
-        #     PI.listen_data = PI.listen()
-        #     PI.listen_bool = False
+#         # if PI.listen_bool is True:
+#         #     PI.listen_data = PI.listen()
+#         #     PI.listen_bool = False
 
-        # if PI.speak_bool is True and PI.scan_input_data is not None:
-        #     PI.speak(PI.scan_input_data)
-        #     if PI.listen() == "done":
-        #         PI.scan_input_data = None
+#         # if PI.speak_bool is True and PI.scan_input_data is not None:
+#         #     PI.speak(PI.scan_input_data)
+#         #     if PI.listen() == "done":
+#         #         PI.scan_input_data = None
 
 
-def HuiFunction():
+# def HuiFunction():
 
-    global scan_input_bool
-    global listen_bool
-    global speak_bool
-    global listen_data
-    global scan_input_data
+#     global scan_input_bool
+#     global listen_bool
+#     global speak_bool
+#     global listen_data
+#     global scan_input_data
 
-    HUI = HuiController()
-    while True:
-        if scan_input_data is None:
-            HUI.show_frame('start')
-            scan_input_bool = True
-            print("Here")
-            HUI.update()
+#     HUI = HuiController()
+#     while True:
+#         if scan_input_data is None:
+#             HUI.show_frame('start')
+#             scan_input_bool = True
+#             print("Here")
+#             HUI.update()
 
-        # if scan_input_data is not None:
-        #     scan_input_bool = False
-        #     HUI.show_frame('choice', scan_input_data)
-        #     HUI.update()
+#         # if scan_input_data is not None:
+#         #     scan_input_bool = False
+#         #     HUI.show_frame('choice', scan_input_data)
+#         #     HUI.update()
         
+
+# # def logic():
+# #     PiProcess = Process(target=PiFunction)
+# #     HuiProcess = Process(target=HuiFunction)
+
+# #     PiProcess.start()
+# #     HuiProcess.start()
 
 def logic():
-    PiProcess = Process(target=PiFunction)
-    HuiProcess = Process(target=HuiFunction)
+    scan_input_bool = True
+    listen_bool = False
+    speak_bool = False
+    listen_data = None
+    scan_input_data = None
 
-    PiProcess.start()
-    HuiProcess.start()
+    HUI = HuiController()
+    PI = PiController(HUI)
+
+    while True:
+        if scan_input_data is None and scan_input_bool is True:
+            HUI.show_frame('start')
+            scan_input_data = PI.scan_input()
+            HUI.update()
+        
+        if scan_input_data is not None and scan_input_bool is True:
+            HUI.show_frame('choice', data=scan_input_data)
+            
+            while HUI.tmp is None:
+                HUI.update()
+
+            if HUI.tmp == True:
+                
+
+
 
 
 if __name__ == '__main__':
-    LogicController = Controller()
     logic()
--- CODE/PI-CODE/ControllerClass.py
+++ CODE/PI-CODE/ControllerClass.py
@@ -4,21 +4,22 @@ import cv2 as cv
 import serial
 from pyzbar.pyzbar import decode
 
-class Controller:
-    scan_input_bool = False
-    listen_bool = False
-    speak_bool = False
+# class Controller:
+#     scan_input_bool = False
+#     listen_bool = False
+#     speak_bool = False
     
-    scan_input_data = None
-    listen_data = None
+#     scan_input_data = None
+#     listen_data = None
 
 
-    def __init__(self) -> None:
-        pass
+#     def __init__(self) -> None:
+#         pass
 
 
-class PiController(Controller):
-    def __init__(self):
+class PiController():
+    def __init__(self, Hui):
+        self.HUI = Hui
         self.listening = False
         self.debug = True
         self.startMarker = '<'
@@ -86,12 +87,13 @@ class PiController(Controller):
         if self.debug:
             print("Camera On")
         while True:
+            self.HUI.update()
             ret, frame = self.vid.read()
             if ret == True:
                 qr_value = self.read_qr(frame)
 
-                if qr_value is None: continue
-                else: return qr_value
+            if qr_value is None: continue
+            else: return qr_value
 
     def speak(self, qr_value):
         self.sendToArduino(qr_value)
@@ -103,71 +105,48 @@ class PiController(Controller):
                 return arduinoReply
         
 
-class HuiController(tk.Tk, Controller):
+class HuiController(tk.Tk):
      def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.tmp = None
+
+        self.container = tk.Frame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        #self.frames['start'] = start(container=self.container, container=self)
-
-        for F in (start, twochoice, mm):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-            frame.grid(row=0, column= 0, sticky= "nsew")
-
-        self.show_frame("start")
-
+        self.frames['start'] = start(parent=self.container, container=self)
+        self.frames['start'].grid(row=0, column= 0, sticky= "nsew")
     
-     def show_frame(self, page_name, data =None):
+     def show_frame(self, page_name, data = None):
         '''Show a frame for the given page name'''
-        frame = self.frames[page_name]
-        frame.tkraise()
 
-       # if page_name == 'twochoice':
-        #  frame.tkraise()
-        #else:
-         #   frame = self.frames[page_name]
-          #  frame.tkraise()
+        if page_name == 'choice':
+            self.create_choice(page_name, data)
+            frame = self.frames[page_name]
+            frame.tkraise()
+        else:
+            frame = self.frames[page_name]
+            frame.tkraise()
 
-        #def create_twochoice(self, page_name, data):
-         #self.frames[page_name] = twochoice(parent=self.container, controller=self)
-          
+     def create_choice(self, page_name, data):
+        self.frames[page_name] = choice(parent=self.container, controller=self)
+        self.frames[page_name].grid(row=0, column= 0, sticky= "nsew")
+
+     def give_bool(self, val):
+        self.tmp = val
+        
 class start(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg= "#d459de")
         self.controller = controller
         label = tk.Label (self, text= "Welcome to PAPI, Please Scan QR", width= 20, height= 5, font= ("Comic Sans Ms",50), bg= "#d459de")    
         controller.attributes('-fullscreen', True)
-        label.pack(side="top", fill= "x", pady=10)   
+        label.pack(side="top", fill= "x", pady=10)    
 
-        button1 = tk.Button (self, text= "Go to ID",
-        command= lambda: controller.show_frame("twochoice"))
-
-        button1.pack(side="bottom", fill= "x",pady=10) 
-
-
-#class twochoice(tk.Frame):
-     #def __init__(self, parent, controller):
-        #tk.Frame.__init__(self, parent)
-        #self.controller = controller
-        #label1 = tk.Label(self, text= "ID#:")
-        #label1.pack(side= "top", fill= "x", pady=10)
-        #Label2 = tk.Label(self, text= "Name Variable Here")
-        #Label2.pack(side= "top", fill= "x", pady=10)
-        #button1 = tk.Button(self, text= "Correct Information",
-        #command= lambda: controller.show_frame("start"))
-        #button1.pack()
-        #button2 = tk.Button(self, text= "Incorrect Information",
-        #command= lambda: controller.show_frame("start"))
-        #button2.pack()
-
-class twochoice(tk.Frame):
+class choice(tk.Frame):
      def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg= "#d459de")
         self.controller = controller
@@ -176,10 +155,10 @@ class twochoice(tk.Frame):
         Label2 = tk.Label(self, text= "Name Variable Here",bg= "#d459de", font= ("Calibri", 50), height= 1)
         Label2.pack(side= "top", fill= "x", pady=5)
         button1 = tk.Button(self, text= "Correct Information", font= ("Copper Black", 20), fg= "green",
-        command= lambda: controller.show_frame("start"))
+        command= lambda: controller.give_bool(True))
         button1.pack(side= "bottom", fill= "x", pady=5)
         button2 = tk.Button(self, text= "Incorrect Information", font= ("Copper Black", 20), fg= "red",
-        command= lambda: controller.show_frame("start"))
+        command= lambda: controller.give_bool(False))
         button2.pack(side= "bottom", fill= "x", pady=5)
         button3 = tk.Button(self, text= "Manual Entry Mode", font=("lato",20),
         command= lambda: controller.show_frame("mm"))
