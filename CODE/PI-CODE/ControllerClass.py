cff68a3e5d12491524e86c4365b90403343a00c8
JarredTD <jarred.desrosiers@yahoo.com>
2022-11-06 22:02:49 -0500

    broke

--- CODE/PI-CODE/ControllerClass.py
+++ CODE/PI-CODE/ControllerClass.py
@@ -104,8 +104,7 @@ class PiController(Controller):
         
 
 class HuiController(tk.Tk, Controller):
-
-    def __init__(self, *args, **kwargs):
+     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         
         container = tk.Frame(self)
@@ -114,21 +113,20 @@ class HuiController(tk.Tk, Controller):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        #self.frames['start'] = start(container=self.container, container=self)
-
-        for F in (start, twochoice, mm):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-            frame.grid(row=0, column= 0, sticky= "nsew")
-
-        self.show_frame("start")
-
+        
+        self.frames['start'] = start(container=container, controller=self)
     
-    def show_frame(self, page_name, data =None):
+     def show_frame(self, page_name, data = None):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
+    
+     def flip_speak():
+        Controller.speak_bool = True
+
+     def set_none():
+        Controller.scan_input_data = None
+
 
        # if page_name == 'twochoice':
         #  frame.tkraise()
@@ -153,35 +151,24 @@ class start(tk.Frame):
         button1.pack(side="bottom", fill= "x",pady=10) 
 
 
-#class twochoice(tk.Frame):
-     #def __init__(self, parent, controller):
-        #tk.Frame.__init__(self, parent)
-        #self.controller = controller
-        #label1 = tk.Label(self, text= "ID#:")
-        #label1.pack(side= "top", fill= "x", pady=10)
-        #Label2 = tk.Label(self, text= "Name Variable Here")
-        #Label2.pack(side= "top", fill= "x", pady=10)
-        #button1 = tk.Button(self, text= "Correct Information",
-        #command= lambda: controller.show_frame("start"))
-        #button1.pack()
-        #button2 = tk.Button(self, text= "Incorrect Information",
-        #command= lambda: controller.show_frame("start"))
-        #button2.pack()
-
-class twochoice(tk.Frame):
+class choice(tk.Frame):
      def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg= "#d459de")
         self.controller = controller
         label1 = tk.Label(self, text= "ID#:", bg= "#d459de", font= ("Calibri",50), height= 1)
         label1.pack(side= "top", fill= "x", pady=5)
+
         Label2 = tk.Label(self, text= "Name Variable Here",bg= "#d459de", font= ("Calibri", 50), height= 1)
         Label2.pack(side= "top", fill= "x", pady=5)
+
         button1 = tk.Button(self, text= "Correct Information", font= ("Copper Black", 20), fg= "green",
         command= lambda: controller.show_frame("start"))
         button1.pack(side= "bottom", fill= "x", pady=5)
+
         button2 = tk.Button(self, text= "Incorrect Information", font= ("Copper Black", 20), fg= "red",
         command= lambda: controller.show_frame("start"))
         button2.pack(side= "bottom", fill= "x", pady=5)
+
         button3 = tk.Button(self, text= "Manual Entry Mode", font=("lato",20),
         command= lambda: controller.show_frame("mm"))
         button3.pack(side= "bottom", fill= "x", pady=5)
@@ -192,90 +179,9 @@ class yes(tk.Tk):
         tk.Frame.__init__(self, parent)
         self.controller = controller
      
+
 class mm(tk.Frame):
      def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg= "#d459de")
         self.controller = controller
-
         #label1 = tk.Label
-#      def __init__(self, *args, **kwargs):
-#         tk.Tk.__init__(self, *args, **kwargs)
-        
-#         container = tk.Frame(self)
-#         container.pack(side="top", fill="both", expand=True)
-#         container.grid_rowconfigure(0, weight=1)
-#         container.grid_columnconfigure(0, weight=1)
-
-#         self.frames = {}
-        
-#         self.frames['start'] = start(parent=container, controller=self)
-    
-#      def show_frame(self, page_name, data = None):
-#         '''Show a frame for the given page name'''
-#         frame = self.frames[page_name]
-#         frame.tkraise()
-    
-#      def flip_speak():
-#         Controller.speak_bool = True
-
-#      def set_none():
-#         Controller.scan_input_data = None
-
-
-#        # if page_name == 'twochoice':
-#         #  frame.tkraise()
-#         #else:
-#          #   frame = self.frames[page_name]
-#           #  frame.tkraise()
-
-#         #def create_twochoice(self, page_name, data):
-#          #self.frames[page_name] = twochoice(parent=self.container, controller=self)
-          
-# class start(tk.Frame):
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent, bg= "#d459de")
-#         self.controller = controller
-#         label = tk.Label (self, text= "Welcome to PAPI, Please Scan QR", width= 20, height= 5, font= ("Comic Sans Ms",50), bg= "#d459de")    
-#         controller.attributes('-fullscreen', True)
-#         label.pack(side="top", fill= "x", pady=10)   
-
-#         button1 = tk.Button (self, text= "Go to ID",
-#         command= lambda: controller.show_frame("twochoice"))
-
-#         button1.pack(side="bottom", fill= "x",pady=10) 
-
-
-# class choice(tk.Frame):
-#      def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent, bg= "#d459de")
-#         self.controller = controller
-#         label1 = tk.Label(self, text= "ID#:", bg= "#d459de", font= ("Calibri",50), height= 1)
-#         label1.pack(side= "top", fill= "x", pady=5)
-
-#         Label2 = tk.Label(self, text= "Name Variable Here",bg= "#d459de", font= ("Calibri", 50), height= 1)
-#         Label2.pack(side= "top", fill= "x", pady=5)
-
-#         button1 = tk.Button(self, text= "Correct Information", font= ("Copper Black", 20), fg= "green",
-#         command= lambda: controller.show_frame("start"))
-#         button1.pack(side= "bottom", fill= "x", pady=5)
-
-#         button2 = tk.Button(self, text= "Incorrect Information", font= ("Copper Black", 20), fg= "red",
-#         command= lambda: controller.show_frame("start"))
-#         button2.pack(side= "bottom", fill= "x", pady=5)
-
-#         button3 = tk.Button(self, text= "Manual Entry Mode", font=("lato",20),
-#         command= lambda: controller.show_frame("mm"))
-#         button3.pack(side= "bottom", fill= "x", pady=5)
-
-
-# class yes(tk.Tk):
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent)
-#         self.controller = controller
-     
-
-# class mm(tk.Frame):
-#      def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent, bg= "#d459de")
-#         self.controller = controller
-#         #label1 = tk.Label
