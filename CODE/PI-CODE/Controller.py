053578e81cdf46513e38d16a742771171a1f38dc
JarredTD <jarred.desrosiers@yahoo.com>
2022-11-07 04:02:14 -0500

    init

--- CODE/PI-CODE/Controller.py
+++ CODE/PI-CODE/Controller.py
@@ -11,40 +11,39 @@ def logic():
     PI = PiController(HUI)
 
     while True:
-        if PI.listen() == "calibrate":
-            PI.speak("<confirmed>")
-            while PI.listen() == "continue":
-                PI.speak(PI.scan_input())
-        else:
-            if scan_input_data is None:
-                HUI.show_frame('start')
-                scan_input_data = PI.scan_input()
-                print(scan_input_data)
-                print("Wtf")
+        # if PI.listen() == "calibrate":
+        #     PI.speak("<confirmed>")
+        #     while PI.listen() == "continue":
+        #         PI.speak(PI.scan_input())
+        # else:
+        if scan_input_data is None:
+            HUI.show_frame('start')
+            scan_input_data = PI.scan_input()
+            # print(scan_input_data)
+            HUI.update()
+        
+        if scan_input_data is not None:
+            scan_input_data_tmp = scan_input_data.split(',')
+            HUI.show_frame('choice', data=scan_input_data_tmp)
+
+            while HUI.tmp is None:
                 HUI.update()
-            
-            if scan_input_data is not None:
-                scan_input_data_tmp = scan_input_data.split(',')
-                HUI.show_frame('choice', data=scan_input_data_tmp)
 
-                while HUI.tmp is None:
-                    HUI.update()
+            if HUI.tmp == True:
+                PI.speak(scan_input_data)
+                print(PI.listen())
+                
+                HUI.tmp = None
+                scan_input_data = None
 
-                if HUI.tmp == True:
-                    PI.speak(scan_input_data)
-                    print(PI.listen())
-                    
-                    HUI.tmp = None
-                    scan_input_data = None
-
-                    HUI.update()
+                HUI.update()
 
 
-                if HUI.tmp == False:
-                    scan_input_data = None
-                    HUI.tmp = None
+            if HUI.tmp == False:
+                scan_input_data = None
+                HUI.tmp = None
 
-                    HUI.update()
+                HUI.update()
 
 
 if __name__ == '__main__':
