63bd0494e9345f85025fbafa1e0fb780e7ccb0ad
JarredTD <jarred.desrosiers@yahoo.com>
2022-11-07 19:11:53 -0500

    init

--- CODE/PI-CODE/Controller.py
+++ CODE/PI-CODE/Controller.py
@@ -18,21 +18,25 @@ def logic():
         # else:
         if scan_input_data is None:
             print(1)
+            print(scan_input_data)
             HUI.show_frame('start')
             scan_input_data = PI.scan_input()
             HUI.update()
         
         if scan_input_data is not None:
             print(2)
+            print(scan_input_data)
             scan_input_data_tmp = scan_input_data.split(',')
             HUI.show_frame('choice', data=scan_input_data_tmp)
 
             while HUI.tmp is None:
                 print(3)
+                print(scan_input_data)
                 HUI.update()
 
             if HUI.tmp == True:
                 print(4)
+                print(scan_input_data)
                 PI.speak(scan_input_data)
                 print(PI.listen())
                 
