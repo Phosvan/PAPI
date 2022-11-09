9f44bbdce4c6737924c3724aac707b635b012ca5
kira <103476132+kjsapheryst@users.noreply.github.com>
2022-11-09 12:41:24 -0500

    updating from .grid to .pack
    update from hui

--- CODE/PI-CODE/ControllerClass.py
+++ CODE/PI-CODE/ControllerClass.py
@@ -99,20 +99,21 @@ class PiController():
 class HuiController(tk.Tk):
      def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        
         self.tmp = None
         self.mm_bool = None
         self.mm_send_bool = None
 
         self.container = tk.Frame(self)
-        self.container.grid(row=1, column=1)
-        #self.container.grid_rowconfigure(0, weight=1)
-        #self.container.grid_columnconfigure(0, weight=1)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
         self.frames['start'] = start(parent=self.container, controller=self)
-        self.frames['start'].grid(row=0, column= 0)#sticky="nsew"
+        self.frames['start'].grid(row=0, column= 0, sticky= "nsew")
         self.frames['manual'] = manual(parent=self.container, controller=self)
-        self.frames['manual'].grid(row=0,column=0)#sticky="nsew"
+        self.frames['manual'].grid(row=0,column=0,sticky="nsew")
     
      def show_frame(self, page_name, data = None):
         '''Show a frame for the given page name'''
@@ -126,7 +127,7 @@ class HuiController(tk.Tk):
 
      def create_choice(self, page_name, data):
         self.frames[page_name] = choice(parent=self.container, controller=self, data=data)
-        self.frames[page_name].grid(row=0, column= 0) #sticky= "nsew"
+        self.frames[page_name].grid(row=0, column= 0, sticky= "nsew")
 
      def give_bool(self, val):
         self.tmp = val
@@ -139,6 +140,7 @@ class HuiController(tk.Tk):
     
         
 class Options():
+    
     options=[
     'hopone',
     'hoptwo',
@@ -156,9 +158,9 @@ class Options():
         userSelect = tk.StringVar()
         userSelect.set(Options.options[0])
         drop_menu = tk.OptionMenu(parent, userSelect, *Options.options)
-        drop_menu.grid(row=5, columnspan=4)
-        amount = tk.Spinbox(parent, from_=0, to=30)
-        amount.grid(row=1, column=1)
+        drop_menu.pack(ipadx=150,ipady=10, anchor=tk.NW)
+        amount = tk.Spinbox(parent, from_=0, to=30, font=(30))
+        amount.pack(ipadx=10,ipady=10, anchor=tk.NE)
 
 
 class manual(tk.Frame):
@@ -170,12 +172,12 @@ class manual(tk.Frame):
         for i in range(10):
             entrys.append(Options(self))
 
-        button1 = tk.Button(self, text= "Simulate Data", font= ("Copper Black", 20), fg= "green",
+        button1 = tk.Button(self, text= "Simulate Data", font= ("Copper Black", 17), fg= "green",
         command= lambda: controller.give_mm_send_bool(True))
-        button1.grid(row=4, columnspan = 4) #side= "right", fill= "x",
-        button2 = tk.Button(self, text= "Cancel", font= ("Copper Black", 20), fg= "green",
+        button1.pack(ipadx=5,ipady=5, expand=True, anchor=tk.S)
+        button2 = tk.Button(self, text= "Cancel", font= ("Copper Black", 20), fg= "red",
         command= lambda: controller.give_mm_send_bool(False))
-        button2.grid(row=5, columnspan = 4) #side= "right", fill= "x",
+        button2.pack(ipadx=5,ipady=5, expand=True, anchor=tk.S)
 
 class start(tk.Frame):
     def __init__(self, parent, controller):
@@ -183,28 +185,28 @@ class start(tk.Frame):
         self.controller = controller
         label = tk.Label (self, text= "Welcome to PAPI, Please Scan QR", width= 20, height= 5, font= ("Comic Sans Ms",50), bg= "#d459de")    
         controller.attributes('-fullscreen', True)
-        label.grid(row=2, column=2)    
+        label.pack(side="top", fill= "x", pady=10)    
         button1 = tk.Button(self, text= "Manual Mode", font= ("Copper Black", 20), fg= "green",
         command= lambda: controller.give_mm_bool(True))
-        button1.grid(row=5, columnspan=4)
+        button1.pack(side= "bottom", fill= "x", pady=5)
 
 class choice(tk.Frame):
      def __init__(self, parent, controller, data):
         tk.Frame.__init__(self, parent, bg= "#d459de")
         self.controller = controller
         label1 = tk.Label(self, text= f"{data[2]}", bg= "#d459de", font= ("Calibri",50), height= 1)
-        label1.grid(row=1, column=1, columnspan=4)
+        label1.pack(side= "top", fill= "x", pady=5)
         Label2 = tk.Label(self, text= f"{data[1]} {data[0]}",bg= "#d459de", font= ("Calibri", 50), height= 1)
-        Label2.grid(row=2, column=1, columnspan=4)
+        Label2.pack(side= "top", fill= "x", pady=5)
         button1 = tk.Button(self, text= "Correct Information", font= ("Copper Black", 20), fg= "green",
         command= lambda: controller.give_bool(True))
-        button1.grid(row=4, column=3, columnspan=4)
+        button1.pack(side= "bottom", fill= "x", pady=5)
         button2 = tk.Button(self, text= "Incorrect Information", font= ("Copper Black", 20), fg= "red",
         command= lambda: controller.give_bool(False))
-        button2.grid(row=5, column=3, columnspan=4)
+        button2.pack(side= "bottom", fill= "x", pady=5)
         button3 = tk.Button(self, text= "Manual Entry Mode", font=("lato",20),
         command= lambda: controller.show_frame("mm"))
-        button3.grid(row=6)
+        button3.pack(side= "bottom", fill= "x", pady=5)
 
 
 class yes(tk.Tk):
